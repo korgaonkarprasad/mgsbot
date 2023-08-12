8d7257a203f814a9c6427ad425dc39b7485a6dcd
Sagar Shinde <sagar.shinde.inc@gmail.com>
2023-08-12 11:12:18 +0530

    project created

---
+++ mgsbot/src/main/java/com/mgs/mgsbot/MgsbotApplication.java
@@ -0,0 +1,13 @@
+package com.mgs.mgsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgsbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgsbotApplication.class, args);
+	}
+
+}
---
+++ mgsbot/src/test/java/com/mgs/mgsbot/MgsbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgs.mgsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgsbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
